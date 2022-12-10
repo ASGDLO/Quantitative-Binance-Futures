3e42a5cba61e33de8b08390b114531809a6bcbf7
Ethan <32297705+ASGDLO@users.noreply.github.com>
2022-12-10 18:01:00 +0900

    enum

--- Quantopianist/freqtrade/enums/rpcmessagetype.py
+++ Quantopianist/freqtrade/enums/rpcmessagetype.py
@@ -5,14 +5,19 @@ class RPCMessageType(Enum):
     STATUS = 'status'
     WARNING = 'warning'
     STARTUP = 'startup'
-    BUY = 'buy'
-    BUY_FILL = 'buy_fill'
-    BUY_CANCEL = 'buy_cancel'
-    SELL = 'sell'
-    SELL_FILL = 'sell_fill'
-    SELL_CANCEL = 'sell_cancel'
+
+    ENTRY = 'entry'
+    ENTRY_FILL = 'entry_fill'
+    ENTRY_CANCEL = 'entry_cancel'
+
+    EXIT = 'exit'
+    EXIT_FILL = 'exit_fill'
+    EXIT_CANCEL = 'exit_cancel'
+
     PROTECTION_TRIGGER = 'protection_trigger'
     PROTECTION_TRIGGER_GLOBAL = 'protection_trigger_global'
+
+    STRATEGY_MSG = 'strategy_msg'
 
     def __repr__(self):
         return self.value
